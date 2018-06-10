72a6b6cddd591e5dd580733a1fdde478e1880026
Nathan Minissale <nathan@Nathans-iMac.local>
2018-06-10 11:49:04 +0800

    updates to enviroment to make it more useful

---
+++ enviroment.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# OS detection
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   platform='mac'
+fi
+
+# export is for testing
+# in production place in /etc/enviroment or ~/.profile
+export PUID=1000
+export PGID=100
+export TZ="Australia/Perth"
+export USERDIR="~/docker"
+export ROOT_PASSWORD="bitterpilot"
+
+# Retrieve ip and export to temporary enviroment variable.
+if [[ $platform == 'linux' ]]; then
+    export IP="$(ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//' | awk '!/[a-z]/')"
+elif [[ $platform == 'mac' ]]; then
+    export IP="$(ipconfig getifaddr en0)"
+fi
+
+# TODO add a switch to direct export to permanent enviroment variables
